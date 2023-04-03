0cb775be05e9b46fd7a23bf6215c337b74855bf4
Bagaji0108 <aranisl0108@gmail.com>
2023-04-03 23:50:40 +0900

    Test

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 public class Path
 {
     [SerializeField]
-    private int x, y; //좌표
+    private int x, y; //좌표.
     [SerializeField]
     private bool isEnemy; // 적이 존재하는가?
 }
