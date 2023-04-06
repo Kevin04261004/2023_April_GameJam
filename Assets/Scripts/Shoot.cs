149b69b536b8f437b5d9fcfe25db9d57b3c88091
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-04-06 13:05:46 +0900

    base setting
    dd

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    public void HandInput()
+    {
+
+    }
+}
