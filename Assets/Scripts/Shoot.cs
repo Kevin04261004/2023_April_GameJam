67852e8d994fd7081dbd71285a66b5716160afad
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-04-06 13:56:58 +0900

    dd
    d

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     [SerializeField]
-    private GameObject bullet;
-
-    private void Awake()
-    {
-        
-    }
+    private Transform BallGroup;
+    [SerializeField]
+    private Vector3 firstPos;
+    [SerializeField]
+    private bool shotable;
     private void Update()
     {
-        
-    }
-    public void HandInput()
-    {
-
+        if(Input.GetMouseButtonDown(0))
+        {
+            firstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(2.5f, 2.5f, 10); // 카메라 위치
+        }
+        shotable = true;
+        for (int i = 0; i < BallGroup.childCount; i++) 
+        {
+            if (BallGroup.GetChild(i).GetComponent<Ball>().isMoving)
+            {
+                shotable = false;
+            }
+        }
+            
     }
 }
