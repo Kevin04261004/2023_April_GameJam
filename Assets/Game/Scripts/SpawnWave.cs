1a75b9f8b471ae315057292647157b91141df070
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-04-09 17:35:44 +0900

    CSV기능 완성!!
    ㅇㅇ

--- Assets/Game/Scripts/SpawnWave.cs
+++ Assets/Game/Scripts/SpawnWave.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
+[System.Serializable]
+public class AllData
+{
+    public StageData[] Stage;
+}
+[System.Serializable]
+public class StageData
+{
+    public int Point0;
+    public int Point1;
+    public int Point2;
+    public int Point3;
+    public int Point4;
+    public int Point5;
+    public int StageID;
+}
 public class SpawnWave : MonoBehaviour
 {
     [SerializeField]
     private Transform[] point;
-    public int Turn =0;
+    public int Turn = 0;
+    public TextAsset data;
+    public AllData datas;
+
+    public int[] SpawnEnemy;
+    private void Awake()
+    {
+        datas = JsonUtility.FromJson<AllData>(data.text); // json 읽어들이기
+    }
     private void Start()
     {
         GameManager.instance.objectpool.Get(1, point[0]);
+        SpawnEnemy = new int[6];
         Turn = 0;
     }
     public int GetTurn()
@@ -28,36 +53,23 @@ public class SpawnWave : MonoBehaviour
             }
         }
         Turn++;
-        switch (Turn) //스테이지 제작 가능. Get(생성할 블록, 생성할 위치)
-                      //1: 일반 블록, 2: 폭탄블록, 3: 더하기블록, 4:체력감소블록, 5: 행동블록
+        // json stage에 따라 읽어들이기
+
+        SpawnEnemy[0] = datas.Stage[Turn - 1].Point0;
+        SpawnEnemy[1] = datas.Stage[Turn - 1].Point1;
+        SpawnEnemy[2] = datas.Stage[Turn - 1].Point2;
+        SpawnEnemy[3] = datas.Stage[Turn - 1].Point3;
+        SpawnEnemy[4] = datas.Stage[Turn - 1].Point4;
+        SpawnEnemy[5] = datas.Stage[Turn - 1].Point5;
+
+
+        for (int i =0; i< SpawnEnemy.Length; i++)
         {
-            case 1:
-                GameManager.instance.objectpool.Get(1, point[1]);
-                GameManager.instance.objectpool.Get(3, point[2]);
-                break;
-            case 2:
-                GameManager.instance.objectpool.Get(3, point[3]);
-                GameManager.instance.objectpool.Get(1, point[5]);
-                break;
-            case 3:
-                GameManager.instance.objectpool.Get(1, point[1]);
-                GameManager.instance.objectpool.Get(3, point[2]);
-                GameManager.instance.objectpool.Get(2, point[3]);
-                GameManager.instance.objectpool.Get(1, point[4]);
-                break;
-            case 4:
-                GameManager.instance.objectpool.Get(2, point[0]);
-                GameManager.instance.objectpool.Get(5, point[2]);
-                GameManager.instance.objectpool.Get(4, point[3]);
-                GameManager.instance.objectpool.Get(1, point[4]);
-                break;
-            case 5:
-                GameManager.instance.objectpool.Get(2, point[0]);
-                GameManager.instance.objectpool.Get(4, point[3]);
-                GameManager.instance.objectpool.Get(1, point[4]);
-                break;
-            default:
-                break;
+            if(SpawnEnemy[i] != 0)
+            {
+                GameManager.instance.objectpool.Get(SpawnEnemy[i], point[i]);
+            }
+            
         }
         GameManager.instance.pathFind.StartFinding();
     }
